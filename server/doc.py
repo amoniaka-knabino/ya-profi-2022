d51913839efda978dbb016de570d738917631adb
Ivanova <cat1dog@yandex.ru>
2022-03-11 12:20:53 +0300

    Revert "fix"
    This reverts commit 9a0dfc67b7d25a2576ad2aed03b155fe53d161aa.

--- server/doc.py
+++
@@ -1,9 +0,0 @@
-from flask_apispec import FlaskApiSpec
-from server import app
-
-docs = FlaskApiSpec(app)
-
-docs.register(notes)
-
-app.add_url_rule('/notes', view_func=StoreResource.as_view('Store'))
-docs.register(StoreResource)
